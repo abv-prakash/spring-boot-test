b858646f05c2447401e83ecdb0b9c1137d88db35
Abhinav Prakash <abhinav.prakash@practo.com>
2017-05-22 01:28:55 +0530

    add some test file

---
+++ src/main/java/hello/GreetMain.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetMain.class, args);
+	}
+
+}
